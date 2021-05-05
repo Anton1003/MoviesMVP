980f47f9bd8663d5aafef7c8faf1664094672b2a
Anton <antonilyin6@gmail.com>
2021-05-05 22:36:05 +0500

    add network in MainPresenter

--- Movies/Movies/MainModule/Presenter/MainPresenter.swift
+++ Movies/Movies/MainModule/Presenter/MainPresenter.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+protocol MainViewProtocol: AnyObject {
+    func succes()
+    func failure()
+}
+
+protocol MainViewPresenterProtocol: AnyObject {
+    var films: Film? { get set }
+    init(view: MainViewProtocol, networkingService: NetworkServiceProtocol)
+    func getFilms()
+}
+
+///
+class MainPresenter: MainViewPresenterProtocol {
+    weak var view: MainViewProtocol?
+    var networkingService: NetworkServiceProtocol!
+    var films: Film?
+
+    required init(view: MainViewProtocol, networkingService: NetworkServiceProtocol) {
+        self.view = view
+        self.networkingService = networkingService
+        getFilms()
+    }
+
+    func getFilms() {
+        networkingService.getMovies { [weak self] film in
+            guard let self = self else { return }
+            self.films = film
+            self.view?.succes()
+        }
+    }
+}
