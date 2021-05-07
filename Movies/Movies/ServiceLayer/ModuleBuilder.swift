26fa7e691071606066d61a31f3417243c1545c08
Anton <antonilyin6@gmail.com>
2021-05-07 15:10:32 +0500

    create main router

--- Movies/Movies/ServiceLayer/ModuleBuilder.swift
+++ Movies/Movies/ServiceLayer/ModuleBuilder.swift
@@ -7,14 +7,14 @@
 
 import UIKit
 
-protocol Builder {
-    static func createMainModule() -> UIViewController
-    static func createDetailModule(film: Result?) -> UIViewController
+protocol BuilderProtocol {
+    func createMainModule() -> UIViewController
+    func createDetailModule(film: Result?) -> UIViewController
 }
 
 ///
-class ModuleBuilder: Builder {
-    static func createMainModule() -> UIViewController {
+class ModuleBuilder: BuilderProtocol {
+    func createMainModule() -> UIViewController {
         let view = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(MoviesController.self)
         let networkService = NetworkService()
         let presenter = MainPresenter(view: view, networkingService: networkService)
@@ -22,7 +22,7 @@ class ModuleBuilder: Builder {
         return view
     }
 
-    static func createDetailModule(film: Result?) -> UIViewController {
+    func createDetailModule(film: Result?) -> UIViewController {
         let view = UIStoryboard(name: "Detail", bundle: nil).instantiateViewController(DetailController.self)
         let networkService = NetworkService()
         let presenter = DetailPresenter(view: view, networkingService: networkService, film: film)
